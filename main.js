ae14728984e88217c61c2f39d3bc3f95efeeb5ea
Heet Shihora <heetshihora04@gmail.com>
2024-04-19 17:58:22 +0530

    added conditions

--- main.js
+++ main.js
@@ -35,17 +35,28 @@ function selectRows(option1, option2) {
         // console.log("No radio buttons found in any rows. Doing nothing.");
     }
 }
-let a = false;
+
 
 if (!(localStorage.option1 && localStorage.option2)) {
-    let option1 = Number(prompt("Enter value for feedback 1 to 5 1-Strongly Agree and 5-Strongly Disagree"));
-    let option2 = Number(prompt("Enter value 1 for Yes and 2 for No"));
+    let option1 = -1;
+    let option2 = -1;
+    while (!(option1 >= 1 && option1 <= 5)) {
+        option1 = Number(prompt("Enter value for feedback 1 to 5 1-Strongly Agree and 5-Strongly Disagree"));
+    }
+
+    while (!(option2 >= 1 && option2 <= 2)) {
+        option2 = Number(prompt("Enter value 1 for Yes and 2 for No"));
+    }
     localStorage.setItem("option1", option1);
     localStorage.setItem("option2", option2);
-    a = true;
+
 }
 
 if (localStorage.option1 >= 1 && localStorage.option1 <= 5 && localStorage.option2 >= 1 && localStorage.option2 <= 2) {
 
     selectRows(localStorage.option1, localStorage.option2);
+}
+else {
+    localStorage.removeItem("option1");
+    localStorage.removeItem("option2");
 }
