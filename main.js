5a5a309c4914d8cfd8aa2f34438c62fe51e64682
Heet Shihora <heetshihora04@gmail.com>
2024-04-19 17:16:59 +0530

    all options added

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function selectRows() {
+function selectRows(option1, option2) {
     var table = document.getElementById("DataTables_Table_0");
 
     if (!table) {
@@ -12,9 +12,13 @@ function selectRows() {
     var radioButtonFound = false;
 
     rows.forEach(function (row) {
-        var firstOption = row.querySelector("input[type='radio']");
-        if (firstOption) {
-            firstOption.checked = true;
+        var allOptions = row.querySelectorAll("input[type='radio']");
+        if (allOptions && allOptions.length == 2) {
+            allOptions[option2 - 1].checked = true;
+            radioButtonFound = true;
+        }
+        else if (allOptions && allOptions.length == 5) {
+            allOptions[option1 - 1].checked = true;
             radioButtonFound = true;
         }
     });
@@ -23,6 +27,7 @@ function selectRows() {
         var submitButton = document.getElementById("cphPageContent_btnSaveFeedback");
         if (submitButton) {
             submitButton.click();
+            // console.log("clicked");
         } else {
             console.error("Submit button with id 'cphPageContent_btnSaveFeedback' not found.");
         }
@@ -30,5 +35,17 @@ function selectRows() {
         // console.log("No radio buttons found in any rows. Doing nothing.");
     }
 }
+let a = false;
 
-selectRows();
+if (!(localStorage.option1 && localStorage.option2)) {
+    let option1 = Number(prompt("Enter value for feedback 1 to 5 1-Strongly Agree and 5-Strongly Disagree"));
+    let option2 = Number(prompt("Enter value 1 for Yes and 2 for No"));
+    localStorage.setItem("option1", option1);
+    localStorage.setItem("option2", option2);
+    a = true;
+}
+
+if (localStorage.option1 >= 1 && localStorage.option1 <= 5 && localStorage.option2 >= 1 && localStorage.option2 <= 2) {
+
+    selectRows(localStorage.option1, localStorage.option2);
+}
